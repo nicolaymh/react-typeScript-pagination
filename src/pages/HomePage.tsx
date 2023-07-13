56932e3bd8d97c1bd48d32fd868e455d454c7433
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-07-12 21:27:02 -0500

    Finished pagination, filtered

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import { Loading } from "../components/Loading";
 import { usePokemon } from "../hooks/usePokemon";
 import { Pokemon } from "../interfaces/fetchAllPokemonResponse";
@@ -6,23 +6,40 @@ import { Pokemon } from "../interfaces/fetchAllPokemonResponse";
 export const HomePage = () => {
    const { isLoading, pokemons } = usePokemon();
    const [currentPage, setCurrentPage] = useState(0);
+   const [search, setSearch] = useState("");
 
    const filterPokemons = (): Pokemon[] => {
-      return pokemons.slice(currentPage, currentPage + 5);
+      if (search.length === 0) pokemons.slice(currentPage, currentPage + 5);
+
+      // There is something in the input
+      const filtered = pokemons.filter((poke) => poke.name.includes(search));
+      return filtered.slice(currentPage, currentPage + 5);
    };
 
    const nextPage = () => {
-      setCurrentPage(currentPage + 5);
+      if (pokemons.filter((poke) => poke.name.includes(search)).length > currentPage + 5)
+         setCurrentPage(currentPage + 5);
    };
 
    const prevPage = () => {
       if (currentPage > 0) setCurrentPage(currentPage - 5);
    };
 
+   const onSearchChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+      setCurrentPage(0);
+      setSearch(target.value);
+   };
+
    return (
       <div className="mt-5">
          <h1>Pokemon List</h1>
          <hr />
+         <input
+            type="text"
+            className="mb-2 form-control"
+            placeholder="Search Pokemon"
+            onChange={onSearchChange}
+         />
          <button className="btn btn-primary" onClick={prevPage}>
             Previous
          </button>
