344fc68b2d9b2487cceafe53f6d80397a5f966b6
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-07-12 20:32:30 -0500

    Added nextPage() and prevPage() functions

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,25 +1,46 @@
+import { useState } from "react";
 import { Loading } from "../components/Loading";
 import { usePokemon } from "../hooks/usePokemon";
+import { Pokemon } from "../interfaces/fetchAllPokemonResponse";
 
 export const HomePage = () => {
    const { isLoading, pokemons } = usePokemon();
+   const [currentPage, setCurrentPage] = useState(0);
+
+   const filterPokemons = (): Pokemon[] => {
+      return pokemons.slice(currentPage, currentPage + 5);
+   };
+
+   const nextPage = () => {
+      setCurrentPage(currentPage + 5);
+   };
+
+   const prevPage = () => {
+      if (currentPage > 0) setCurrentPage(currentPage - 5);
+   };
 
    return (
       <div className="mt-5">
          <h1>Pokemon List</h1>
          <hr />
-
+         <button className="btn btn-primary" onClick={prevPage}>
+            Previous
+         </button>
+         &nbsp;
+         <button className="btn btn-primary" onClick={nextPage}>
+            Next
+         </button>
          <table className="table">
             <thead>
                <tr>
-                  <th>ID</th>
-                  <th>Name</th>
+                  <th style={{ width: 100 }}>ID</th>
+                  <th style={{ width: 100 }}>Name</th>
                   <th>Image</th>
                </tr>
             </thead>
 
             <tbody>
-               {pokemons.map(({ id, name, pic }) => (
+               {filterPokemons().map(({ id, name, pic }) => (
                   <tr key={id}>
                      <td>{id}</td>
                      <td>{name}</td>
@@ -30,7 +51,6 @@ export const HomePage = () => {
                ))}
             </tbody>
          </table>
-
          {isLoading && <Loading />}
       </div>
    );
